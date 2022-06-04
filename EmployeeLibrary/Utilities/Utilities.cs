9b3ae81e25afdd09ec7d6f1746fc6292946126b0
garrepalliprasad <garrepalli.prasad@gmail.com>
2022-06-04 19:49:32 +0530

    Added CKKS parameters in Utilities

---
+++ EmployeeLibrary/Utilities/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Research.SEAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeLibrary.Utilities
+{
+    public class Utilities
+    {
+        public Utilities()
+        {
+            EncryptionParameters parms = new EncryptionParameters(SchemeType.CKKS);
+            ulong polyModulusDegree = 8192;
+            parms.PolyModulusDegree = polyModulusDegree;
+            parms.CoeffModulus = CoeffModulus.Create(polyModulusDegree, new int[] { 60, 40, 40, 60 });
+            SEALContext context = new SEALContext(parms);
+            KeyGenerator keygen = new KeyGenerator(context);
+            SecretKey secretKey = keygen.SecretKey;
+            keygen.CreatePublicKey(out PublicKey publicKey);
+            keygen.CreateRelinKeys(out RelinKeys relinKeys);
+            Encryptor encryptor = new Encryptor(context, publicKey);
+            Evaluator evaluator = new Evaluator(context);
+            Decryptor decryptor = new Decryptor(context, secretKey);
+            CKKSEncoder encoder = new CKKSEncoder(context);
+        }
+    }
+}
